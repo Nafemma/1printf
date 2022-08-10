deca548d90d14e2d6f9ab318bc1188158233bfa4
AmfumuJoe <joekima98@gmail.com>
2022-08-10 03:11:15 +0200

    function that produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _printf - Function receives main string and all the necessary parameters to
+ * print a formated string
+ * @format: String contains all the desired characters
+ * Return: Total count of the characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+	/*
+	*make an array of a convert structure
+	*allows us to call functions through pointers whenever
+	*there is a match to a char
+	*/
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{"r", print_reversed},
+		{"R", rot13},
+		{NULL, NULL}
+	};
+	/* declare variable length variable*/
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+
+	/* initialize the variable */
+	va_start(arg_list, format);
+	/*Call parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
+}
